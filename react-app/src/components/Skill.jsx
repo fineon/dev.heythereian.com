9328d03b6888f1f0f62923a5ea1b8edc042259c7
Ian <ian@heythereian.com>
2021-03-04 21:24:00 -0800

    added more react components and code splitting, imported all data ready to be inserted

--- react-app/src/components/Skill.jsx
+++ react-app/src/components/Skill.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import '../components/asset-import';
+import jsTools from '../components/asset-import';
+
 
 export default function Skill() {
     return (
+        // nested map for all the skill section?
         <>
+            <section className="skill">
+                <div className="skill__container">
+                    <div className="skill__container__unit">
+                        <hr className="skill__divider" />
+                        <h2 id="webdev" className="skill__container__unit__title">Web Developer</h2>    
+                        <ul className="skill__container__unit__list">
+                        <li className="skill__container__unit__list__item">
+                        Fully responsive layout development for mobile-first, tablet and desktop layouts
+                    </li>
+                    <li className="skill__container__unit__list__item">
+                        Web hosting management: CNAME record, Cpanel admin dashboard, domain name management
+                    </li>
+                    <li className="skill__container__unit__list__item">
+                        Tools: HTML, CSS, Javascript, Node.js, WordPress
+                    </li>
+                            </ul>                
+                    </div>
+
+                    <div className='skill__container--2'>
+                    {jsTools.map(item => {
+                            return (
+                               <div 
+                               key={item.id}
+                               className="skill__container--2__square">
+                                   <img src={item.img} alt="icon" className="skill__container--2__img"/>
+                                   <p className="skill__container--2__text">
+                                       {item.desc}
+                                   </p>
+                               </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </section>
+
 
         </>
     )
---
+++ react-app/src/components/asset-import.js
@@ -0,0 +1,56 @@
+import reactIcon from '../assets/react.png';
+import sassIcon from '../assets/sass.png';
+import highChartsIcon from '../assets/highcharts.png';
+import expressIcon from '../assets/expressjs.png';
+
+import codashImg from '../assets/codash19.jpg'
+import designDemoImg from '../assets/webdesigndemo.jpg'
+
+import shopifyWeb from '../assets/shopifydemo.jpg'
+
+
+ const jsTools = [
+    {
+        img: sassIcon,
+        desc: 'SASS Pre-processing for CSS. Style your website that takes 2x        faster than normal CSS',
+        id: 0
+    },
+    {
+        img: reactIcon,
+        desc: 'React.js creates a single page app that loads blazing fast and        responsive to user interactions',
+        id: 1
+    },
+    {
+        img: expressIcon,
+        desc: 'Express.js creates a server suits for classic MVC design model and backend services',
+        id: 2
+    },
+    {
+        img: highChartsIcon,
+        desc: 'Highcharts.js visualizes data that is responsive and loads fast for web pages',
+        id: 3
+    },
+]
+
+const designAssets = [
+    {
+        img: codashImg,
+        desc: 'Express.js creates a server suits for classic MVC design model and backend services',
+        id: 2
+    },
+    {
+        img: designDemoImg,
+        desc: 'Highcharts.js visualizes data that is responsive and loads fast for web pages',
+        id: 3
+    },
+]
+
+const shopifySkill = [
+    {
+        img: shopifyDemo,
+        desc: 'Highcharts.js visualizes data that is responsive and loads fast for web pages',
+        id: 3
+    },
+]
+
+export default jsTools && designAssets && shopifySkill
